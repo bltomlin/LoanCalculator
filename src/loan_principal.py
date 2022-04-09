58deacb606de1ac043b2aad241bcda6ac66ef909
Brendan Tomlinson <62484274+bltomlin@users.noreply.github.com>
2022-04-08 23:34:10 -0700

    Added comments
    Comments added for description.

--- src/loan_principal.py
+++ src/loan_principal.py
@@ -1,3 +1,14 @@
+"""
+This file contains the logic for calculating the monthly payment
+when the period of the loan is known.
+"""
+
 def loan_principal(annuity_payment, nominal_interest):
+	"""
+	Calculates fixed monthly payment.
+
+	:param: loan_input: Dollar amount of the loan
+	:return: monthly payment required by user
+	"""
 	loan_principal = annuity_payment * ( (i * pow((1 + i), n) / (pow((1 + i), n) - 1) ))
 	return loan_principal
