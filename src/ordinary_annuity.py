49efbde1a21d3cd5eca0f7a34f8d17c23885bfdb
Brendan Tomlinson <62484274+bltomlin@users.noreply.github.com>
2022-04-09 00:42:53 -0700

    Returned ordinary annuity value
    I forgot to return the value.

--- src/ordinary_annuity.py
+++ src/ordinary_annuity.py
@@ -8,6 +8,7 @@ def ordinary_annuity(loan_input, nominal_interest):
 
 	:param: loan_input: dollar amount of the loan
 	:param: nominal_interest: monthly interest rate
-	:return: void
+	:return: total cost of loan
 	"""
-	ordinary_annuity = loan_input * ( (i * pow((1 + i), n) / (pow((1 + i), n) - 1) )
+	ordinary_annuity = loan_input * ((i * pow((1 + i), n) / (pow((1 + i), n) - 1)))
+	return ordinary_annuity
