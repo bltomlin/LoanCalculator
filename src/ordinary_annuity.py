bd7d2d09e8a72667541d0bb12f9779764fc1a14d
Brendan Tomlinson <62484274+bltomlin@users.noreply.github.com>
2022-04-08 23:33:44 -0700

    Added comments
    Comments added for description.

--- src/ordinary_annuity.py
+++ src/ordinary_annuity.py
@@ -1,2 +1,13 @@
+"""
+This file contains the logic for calculating the ordinary annuity.
+"""
+
 def ordinary_annuity(loan_input, nominal_interest):
+	"""
+	Calculates the ordinary annuity.
+
+	:param: loan_input: dollar amount of the loan
+	:param: nominal_interest: monthly interest rate
+	:return: void
+	"""
 	ordinary_annuity = loan_input * ( (i * pow((1 + i), n) / (pow((1 + i), n) - 1) )
