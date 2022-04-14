1772a5563e9040223fa6b71884fd37b7b5bdd727
Brendan Tomlinson <62484274+bltomlin@users.noreply.github.com>
2022-04-13 23:11:29 -0700

    imported new files, added new logic
    New logic was needed to prompt the user input based on what they want to calculate in the program. Imports were modified due to new files and outdated files being removed from the application.

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@
 This file contains the main entry point for the program that
 dictates all logical flow.
 """
-from src import period_calculator, how_long_calculator
+from src import what_calculate, number_of_payments_calculator, ordinary_annuity_calculator, loan_principal_calculator
 
 def loan_calculator():
 	"""
@@ -10,20 +10,38 @@ def loan_calculator():
 
 	:return: void
 	"""
-
-	print("Enter the loan principal:")
-	loan_input = int(input())
-
-	print("What do you want to calculate?")
-	print('type "m" - for number of monthly payments,')
-	print('type "p" - for the monthly payment:')
-	type_input = str(input())
-
-	if type_input == "m":
-		period_calculator()
+	type_input = what_calculate()
+	
+	if type_input == "n":
+		print("Enter the loan principal:")
+		loan_input = int(input())
+		print("Enter the monthly payment:")
+		n = int(input())
+		print("Enter the loan interest:")
+		i = float(input())
+		number_of_payments = math.ceil(number_of_payments_calculator(loan_input, n , i))
+		remainder = int(number_of_payments % 12)
+		years = int(number_of_payments / 12)
+		print(f"It will take {years} years and {remainder} months to repay this loan!")
+	elif type_input == "a":
+		print("Enter the loan principal:")
+		loan_input = int(input())
+		print("Enter the number of periods:")
+		n = int(input())
+		print("Enter the loan interest:")
+		i = float(input())
+		orindary_annuity = ordinary_annuity_calculator(loan_input, n , i)
+		print(f"Your monthly payment = {orindary_annuity}")
 	else:
-	    how_long_calculator(loan_input)
+		print("Enter the annuity payment:")
+		annuity_input = float(input())
+		print("Enter the number of periods:")
+		n = int(input())
+		print("Enter the loan interest:")
+		i = float(input())
+		loan_principal = loan_principal_calculator(annuity_input, n , i)
+		print(f"Your loan principal = {loan_principal}!")
 	    
 
 if __name__ == '__main__':
-    honest_calculator()
+    loan_calculator()
