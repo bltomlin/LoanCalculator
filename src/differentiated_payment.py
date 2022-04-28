49b60d3a36b262a70c549de7f60a0d382de6fa4f
Brendan Tomlinson <62484274+bltomlin@users.noreply.github.com>
2022-04-27 17:11:09 -0700

    renamed variables | added comments
    I renamed the variables to improve code readability. Added comments to explain what this function does.

--- src/differentiated_payment.py
+++ src/differentiated_payment.py
@@ -1,3 +1,15 @@
-def differentiated_payment(P, i, n, m):
-    d = (P / n) + i * (P - ( (P * (m - 1)) / n))
-    return d
+"""
+This file contains the logic to calculate a differentiated payment.
+"""
+
+def differentiated_payments_calculator(loan_input, n, i, m):
+    """
+    Calculates differentiated payment.
+
+    :param: loan_input: principal loan
+    :param: n: number of payments on the loan
+    :param: i: monthly interest rate
+    :return: calculated last payment
+    """
+    differentiated_payment = (loan_input / n) + i * (loan_input - ( (loan_input * (m - 1)) / n))
+    return differentiated_payment
