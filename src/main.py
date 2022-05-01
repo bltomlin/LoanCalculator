63fc736abd25028234a63c80e97a6c87f6a19131
Brendan Tomlinson <62484274+bltomlin@users.noreply.github.com>
2022-05-01 00:23:57 -0700

    added parser arguments and set data types
    I individually added parser arguments because I was getting an error thrown when trying to add them in a single line. I also added types when calling methods to convert strings into the correct data type.

--- src/main.py
+++ src/main.py
@@ -12,20 +12,24 @@ def loan_calculator():
 	:return: void
 	"""
 	parser = argparse.ArgumentParser(description="This program calculates loan payments.")
-	parser.add_argument("-type", "--payment", "--principal", "--periods", "--interest") 
+	parser.add_argument("--type")
+	parser.add_argument("--payment")
+	parser.add_argument("--principal") 
+	parser.add_argument("--periods") 
+	parser.add_argument("--interest") 
 	args = parser.parse_args()
 	data = [args.payment, args.principal, args.periods, args.interest]
 	if args.type == "annuity":
 		if args.principal != None and args.payment != None and args.interest != None:
-			number_of_payments = math.ceil(number_of_payments_calculator(args.principal, args.payment , args.interest))
+			number_of_payments = math.ceil(number_of_payments_calculator(int(args.principal), int(args.payment) , float(args.interest)))
 			remainder = int(number_of_payments % 12)
 			years = int(number_of_payments / 12)
 			print(f"It will take {years} years and {remainder} months to repay this loan!")
 		elif args.principal != None and args.periods != None and args.interest != None:
-			orindary_annuity = ordinary_annuity_calculator(args.principal, args.periods , args.interest)
+			orindary_annuity = ordinary_annuity_calculator(int(args.principal), int(args.periods) , float(args.interest))
 			print(f"Your monthly payment = {orindary_annuity}")
 		else:
-			loan_principal = loan_principal_calculator(args.annuity, args.periods , args.interest)
+			loan_principal = math.floor(loan_principal_calculator(int(args.payment), int(args.periods) , float(args.interest)))
 			print(f"Your loan principal = {loan_principal}!")
 
 
