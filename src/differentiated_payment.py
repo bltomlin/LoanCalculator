b2cce1795411f469839aaa6641e3a8758fedde90
Brendan Tomlinson <62484274+bltomlin@users.noreply.github.com>
2022-05-01 22:28:00 -0700

    interest calculations and iterative logic
    updated logic in the method

--- src/differentiated_payment.py
+++ src/differentiated_payment.py
@@ -11,5 +11,14 @@ def differentiated_payments_calculator(loan_input, n, i, m):
     :param: i: monthly interest rate
     :return: calculated last payment
     """
-    differentiated_payment = (loan_input / n) + i * (loan_input - ( (loan_input * (m - 1)) / n))
-    return differentiated_payment
+    m = 1
+    i = i / (12 * 100)
+    differentiated_payment_total = 0
+    loan_principal = math.floor(loan_principal_calculator(loan_input, n, i))
+    while m <= n:
+	    differentiated_payment = math.floor((loan_principal / n) + i * (loan_principal - ( (loan_principal * (m - 1)) / n)))
+	    differentiated_payment_total += differentiated_payment
+	    print("Month " + str(m) + ": payment is " + str(differentiated_payment))
+	    m += 1
+    overpayment = math.floor(differentiated_payment_total - loan_principal)
+    print("Overpayment = " + str(overpayment))
